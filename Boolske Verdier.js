cf297847c04594614dc0a20085bc19bc6c41c698
DitlevChrOlrik <ditlev@chrolrik.com>
2025-01-21 10:41:31 +0100

    del 4 + 5

---
+++ Boolske Verdier.js
@@ -0,0 +1,47 @@
+// Del 5 // bool og evaluering
+
+// Oppgave 1
+
+console.log(1 === 1) // true
+
+console.log(1 === 2) // false
+
+console.log(3==3) // true
+
+console.log(2==3) // false
+
+// Oppgave 2
+
+console.log(1 == ’1’) // true
+
+console.log(2 === ’2’) // false
+
+// Når det er === forventer denn akkuratt samme data type på begge sider
+
+// Oppgave 3
+
+console.log(1==1 && 2==2) // true
+
+console.log(1===’1’ && 2===2) // false
+
+console.log(1===1 && ‘2’ ===2) //false
+
+console.log(1==’1’ && 2==’2’) // true
+
+// Oppgave 4
+
+console.log(1===’1’ || 2===2) // true
+
+console.log(2==’2’ || 1===’1’) // true
+
+console.log(2===2 || 1===1 || 3===’3’) // true
+
+// Oppgave 5
+
+console.log(1 !== 1) // true
+
+console.log(2 !== 1) // true
+
+console.log(2 !== 1 && 1 === 1) //true
+
+console.log(2 !== ‘2’ && 1=== ‘1’ || 2==’2’ ) //true
---
+++ block_scope.js
@@ -0,0 +1,85 @@
+// Del 4 // Block/scope
+
+// Oppgave 1
+
+{
+    const i = 10;
+    console.log(`i = ${i}`);
+}
+
+// Ut skriften blirn i = 10
+// Grunnen til at dette skjer er på grunn av console.log() gjør skriften til tekst
+
+// Oppgave 2
+
+const x = 10
+{
+    console.log (`i = ${x}`);
+}
+
+// i = 10
+// const blir definert uttafor et parangtes som betyr at den gjelder hele koden
+
+// Oppgave 3
+
+{
+    const y = 10;
+}
+console.log(`y = ${y}`)
+
+//  error
+// const blir definert innenfor et parantes i denne oppgaven som betyr at console log ikke kan lese den
+
+// Oppgave 4
+
+let z;
+{
+    z = 10;
+    // A
+    console.log(`z = ${z}`);
+}
+    // B
+console.log(`z = ${z}`)
+
+/*
+ A for utprintet z = 10
+ B for utprintet z = 10
+
+ fordi variablen er deklarert utenfor er den ikke begrensa innafor parangtes
+*/
+
+// Oppgave 5
+
+let a;
+fubar();
+console.log(`a = ${a}`)
+
+function fubar()
+{
+    a =2;
+}
+
+// utskrift blir =2
+// fordi a ble deklaret som let, en funksjon ble kalt og funksjonen endrer a til 2. 
+// console.log skriver a = 2
+
+//Oppgave 6
+
+let b = 10;
+
+add(b, 5);
+console.log(b);
+
+function add(c, d)
+{
+let result = c + d;
+console.log(result);
+}
+
+/*
+Utskriften blir 2 utskrifter
+10 og 15
+
+funksjonen kjører b + 5 som er 15.
+console.log leser bare b
+*/
